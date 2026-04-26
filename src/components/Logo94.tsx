15cef3cf639d5132ab221e55db39ab8ed10bd280
Iprok <iprok.new@yandex.ru>
2026-04-26 05:52:45 +0300

    update admin

--- src/components/Logo94.tsx
+++ src/components/Logo94.tsx
@@ -3,13 +3,7 @@ type Props = { compact?: boolean };
 export default function Logo94({ compact = false }: Props) {
   return (
     <div className={`logo94 ${compact ? 'compact' : ''}`}>
-      <div className="logo-main">
-        <span className="logo-nine">9</span>
-        <span className="logo-slash" />
-        <span className="logo-four">4</span>
-      </div>
-      <div className="logo-word"><span>К</span><span>Л</span><span>У</span><span>Б</span></div>
-      {!compact && <div className="logo-tagline"><span>сообщество</span><b>•</b><span>рост</span><b>•</b><span>вдохновение</span></div>}
+      <img className="logo-img" src="/assets/logo_site.png" alt="94 Club" />
     </div>
   );
 }
