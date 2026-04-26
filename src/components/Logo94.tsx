1b44e4804313ceaa0fcf545a038a3f41851f0427
Iprok <iprok.new@yandex.ru>
2026-04-26 06:00:11 +0300

    update admin

--- src/components/Logo94.tsx
+++ src/components/Logo94.tsx
@@ -1,9 +1,11 @@
 type Props = { compact?: boolean };
 
+import logo from '../assets/logo_site.png';
+
 export default function Logo94({ compact = false }: Props) {
   return (
     <div className={`logo94 ${compact ? 'compact' : ''}`}>
-      <img className="logo-img" src="/assets/logo_site.png" alt="94 Club" />
+      <img className="logo-img" src={logo} alt="94 Club" />
     </div>
   );
 }
